02f61793a838f7804a94b0e3a618418d5f38dc90
♪ hiljusti 🎮 <hiljusti@amazon.com>
2021-02-17 00:07:30 -0800

    rename test generation, change from 1 billion to 1 million

--- src/gen_input.cr
+++ src/gen_input.cr
@@ -1,12 +1,12 @@
 NUCLEOTIDES = "ACGT"
 
-def write_a_billion_dna(filename : String)
-  (1..1_000_000).each do |_|
+def write_a_million_dna(filename : String)
+  (1..1_000).each do |_|
     dna = (1..1_000).map{ |_| NUCLEOTIDES[Random.rand(0..3)] }.join
     File.write(filename, dna, mode: "a")
   end
 end
 
-write_a_billion_dna "a.txt"
-write_a_billion_dna "b.txt"
+write_a_million_dna "a.txt"
+write_a_million_dna "b.txt"
 
